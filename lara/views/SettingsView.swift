93ba28c3e7f0e8686fc818dc0034c2425c084d14
neon <1234tvvt@gmail.com>
2026-04-22 11:20:20 +0300

    remove skadz from credits

--- lara/views/SettingsView.swift
+++ lara/views/SettingsView.swift
@@ -502,34 +502,6 @@ struct SettingsView: View {
                         }
                     }
 
-                    HStack(alignment: .top) {
-                        AsyncImage(url: URL(string: "https://github.com/skadz108.png")) { image in
-                            image
-                                .resizable()
-                                .scaledToFill()
-                        } placeholder: {
-                            ProgressView()
-                        }
-                        .frame(width: 40, height: 40)
-                        .clipShape(Circle())
-                        
-                        VStack(alignment: .leading) {
-                            Text("Skadz")
-                                .font(.headline)
-                            
-                            Text("Implemented the respring script in Swift")
-                                .font(.subheadline)
-                                .foregroundColor(Color.secondary)
-                        }
-                        
-                        Spacer()
-                    }
-                    .onTapGesture {
-                        if let url = URL(string: "https://github.com/skadz108"),
-                           UIApplication.shared.canOpenURL(url) {
-                            UIApplication.shared.open(url)
-                        }
-                    }
                 } header: {
                     Text("Credits")
                 }
